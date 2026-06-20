18c67da00c6a838710490513758be6c02a926d34
gabripian <gabrielepianigiani28@gmail.com>
2026-06-20 18:07:45 +0200

    updatted comment in config.py

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# carica backend/.env
+# load backend/.env
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
